6737bc1aca39a3b1df751e4a45277753443c8539
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2024-08-28 18:46:20 +0200

    ADD options to split_data.sh (output file name, output file delimiter, column number(s)) | .

--- split_data.sh
+++ split_data.sh
@@ -1,64 +1,93 @@
 #!/bin/bash
 
-dataset_name="data" 
-
 # Set variables
 
-while [[ $# -gt 0 ]]; do 
+while [[ $# -gt 0 ]]; do
 
-   key="$1"                      
+   key="$1"
    case $key in
-      -n) dataset_name="$2"
+      -f|--file)
+          filename="$2"
           shift 2
           ;;
-      *) if [[ $key = *.csv ]]; then 
-             file_name="$key"
-         elif [[ $key == ?(-)+([0-9]) ]]; then 
-             nb_line="$key"
-         else 
-              echo "Unrecognized option $key"
-              exit 1
-         fi
-         shift
-         ;;
+      -l|--lines)
+          lines="$2"
+          shift 2
+          ;;
+      -c|--columns)
+          columns="$2"
+          shift 2
+          ;;
+      -o|--output)
+          outputname="$2"
+          shift 2
+          ;;
+      -d|--delimiter)
+          delimiter="$2"
+          shift 2
+          ;;
+      *)
+          echo "Unrecognized option $key"
+          exit 1
+          ;;
    esac
 done
 
-if [[ -z ${file_name+x} ]]; then 
-    echo "Enter the name of the file to be split (must have a csv extension)"
+if [[ -z ${filename+x} ]]; then
+    echo "-f: Enter the name of the file to be split (must be tab-separated)"
     exit 1
 fi
 
-if [[ -z ${nb_line+x} ]]; then 
-    echo "Enter the maximum number of lines per new file"
+if [[ -z ${lines+x} ]]; then
+    echo "-l: Enter the maximum number of lines per new file"
     exit 1
 fi
 
-# Keep latitude/longitude columns
-# GBIF data: 
+if [[ -z ${columns+x} ]]; then
+    echo "-c: Enter column number(s), the format must be number or number-number"
+    exit 1
+fi
+
+DIR="$(dirname "${filename}")"
+mkdir "${DIR}/split/"
+if [[ -z ${outputname+x} ]]; then
+    outputname="$(basename "${filename}")"
+    outputname="${DIR}/split/${outputname%.*}" 
+    echo "-o ${outputname}_split will be the prefix of the output file names"
+else
+    outputname="${DIR}/split/${outputname}"
+fi
+
+if [[ -z ${delimiter+x} ]]; then 
+    delimiter="\t"
+    echo "-d ${delimiter} will be the delimiter of the output file"
+fi
+
+
+# Keep ${columns} columns
 #- delimiter: tabulation, use default -d for cut
-#- latitude / longitude: columns 22-23
+#- latitude / longitude: columns 22-23 of GBIF file
 
-cut -f 22-23 "${file_name}" > "${dataset_name}_locations.csv"
+cut -f "${columns}" "${filename}" > "${outputname}2split.tsv"
 
 # Delete header & Add indexes
+# -b a : number all lines, including empty lines
+# -v 0 : first line number = 0
+# -w 1 : column for line numbers = 1
 
-sed 1d "${dataset_name}_locations.csv" | nl -w1 -s"," > "${dataset_name}_locations_idx.csv" 
+sed 1d "${outputname}2split.tsv" | nl -w1 -b a -v 0 > "${outputname}2split_idx.tsv" 
 
-# Split into nb_lines-line files
+# Split into ${lines}-line files
 
-split -l "${nb_line}" --numeric-suffixes --verbose -a 4 "${dataset_name}_locations_idx.csv" "${dataset_name}_split"
-rm "${dataset_name}_locations_idx.csv"
+split -l "${lines}" --numeric-suffixes --verbose -a 4 "${outputname}2split_idx.tsv" "${outputname}_split"
+rm "${outputname}2split_idx.tsv"
 
 # Add a header to all new files
 
-header="index,$(head -n 1 ${dataset_name}_locations.csv)"
-sed -i "1i$header" "${dataset_name}"_split*
+header="index\t$(head -n 1 ${outputname}2split.tsv)"
+sed -i "1i$header" "${outputname}"_split*
 
-# Convert to comma-separated value file
-
-sed -i 's/\t\+/,/g' "${dataset_name}"_split* 
-
-# Add a .csv extension (unnecessary)
-
-#find . -name "*_split*" -exec mv {} {}.csv \; 
+if [[ ${delimiter} != '\t' ]]; then
+    # Convert to ${delimiter}-separated value file
+    sed -i "s/\t\+/${delimiter}/g" "${outputname}"_split* 
+fi
