43a04d161635fdb7a5ed949b2f09a2bde2e04a8a
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2024-08-30 16:28:33 +0200

    FIX (add astype('string')) & ADD warning documentation convertdatetype.py | marinedb/filters/

--- marinedb/filters/convertdatetype.py
+++ marinedb/filters/convertdatetype.py
@@ -3,7 +3,13 @@ import pandas as pd
 
 def apply(df, key):
 
-    df[key] = pd.to_datetime(df[key], format='mixed', yearfirst=True, dayfirst=False, errors='coerce', utc=True) #errors='coerce':
+    #Warning : If the function is applied to a column containing unprocessed date intervals,
+    #          the result may be unexpected:
+    #          - date intervals may be replaced by missing values (errors='coerce')
+    #          - pd.datetime() may interpret the second date as the time (e.g. 2021-03-02/2021-06-02 becomes '2021-03-02 20:21:00-02:00')
+    #          - other special cases may arise that we haven't observed
+
+    df[key] = pd.to_datetime(df[key].astype('string'), format='mixed', yearfirst=True, dayfirst=False, errors='coerce', utc=True) #errors='coerce':
                                                                                                                  #    invalid parsing set as NaT (Not a Time)
                                                                                                                  #    e.g. NaT if date < 1677-09-22 or date > 2262-04-11 (outside al>
                                                                                                                  #    Remark : time span can been wider with unit > ns (ms, s ...)
@@ -14,6 +20,6 @@ def apply(df, key):
                                                                                                                  #format='mixed':
                                                                                                                  #    infer the format for each element individually
 
-    df[key]=df[key].dt.tz_localize(None) # remove the time zone information and preserve local time
+    df[key]=df[key].dt.tz_localize(None) # remove the time zone information (and preserve local time)
 
     return df
