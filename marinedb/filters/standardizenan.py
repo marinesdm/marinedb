0046b977f384751b4c97723a40cb15749cac3fe6
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2024-09-01 11:35:28 +0200

    ADD a parameter to support encodings for NaN other than the default encoding (standardizenan.py) | marinedb/filters/

--- marinedb/filters/standardizenan.py
+++ marinedb/filters/standardizenan.py
@@ -1,22 +1,26 @@
 import numpy as np
+import pandas as pd
 
-def isnan(value):
+def isnan(value, nan_encoding=[]):
 
     if str(value)=='':
         return True
+    if len(nan_encoding)!=0:
+        if (str(value) in nan_encoding):
+            return True
     try:
         return pd.isnull(float(value)) #NaN, nan, 'nan', 'NaN' ...
     except (ValueError,TypeError):
         return pd.isnull(value) #NaT
 
-def apply(df, key=None):
+def apply(df, key=None, nan_encoding=[]):
 
     visnan = np.vectorize(isnan)
 
     if key is None:
         # Convert all missing value in the dataset to pd.NA
-        df = np.where(visnan(df), pd.NA, df)
+        df = pd.DataFrame(np.where(visnan(df,nan_encoding=nan_encoding), pd.NA, df),columns=df.columns)
     else:
-        df[key]=np.where(visnan(df[key]), pd.NA, df[key])
+        df[key]=np.where(visnan(df[key], nan_encoding=nan_encoding), pd.NA, df[key])
 
     return df
