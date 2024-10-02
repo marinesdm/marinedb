144c80176d311e3f1adb866154b426a65b5dfd70
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2024-10-02 20:13:01 +0200

    ADD command-line arguments, ADD display_progress() & MODIFY to store processing time in island.py | .

--- island.py
+++ island.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import glob
+import argparse
 import time
 import random
 from multiprocessing import Pool
@@ -14,6 +15,7 @@ from mpl_toolkits.basemap import Basemap
 from matplotlib.path import Path
 import pathlib
 import socket
+from functools import partial
 
 
 _path = pathlib.Path(__file__).parent.resolve()
@@ -168,16 +170,17 @@ def is_land(lat,lon):
     """
     lat_i = lat_to_index(lat)
     lon_i = lon_to_index(lon)
-    
-    print(f"    First pass | Using GLOBE mask")
+
+    print(f"    STEP N°1 | Using GLOBE mask")
     land_points = np.logical_not(_mask[lat_i,lon_i])
-    
+
     coastline_i = np.where(_mask[lat_i,lon_i]==2)[0]
+    print(f"    {len(coastline_i)} observations on coast")
     if len(coastline_i)!=0:
-        print(f"    {len(coastline_i)} observations on coast | Using basemap & GSHHS coastline data")
+        print(f"    STEP N°2 | Using basemap & GSHHS coastline data")
         land_points[coastline_i] = is_land_basemap(lat[coastline_i],lon[coastline_i])
-        
-    df = pd.DataFrame(land_points, columns=["is_land"]) 
+
+    df = pd.DataFrame(land_points, columns=["is_land"])
     df["latitude"]=lat
     df["longitude"]=lon
     df["mask"]=_mask[lat_i,lon_i]
@@ -185,39 +188,102 @@ def is_land(lat,lon):
     return df
 
 
-def process_one_file(f):
-    
-    start_time = time.time()
-    
-    base=os.path.basename(f)
-    hostname=socket.gethostname().split(".")[0]
-    
-    res = os.path.join(_path,'processed/',f"{base}_{hostname}")
-    #if os.path.exists(res):
+def process_one_file(filepath, sep='\t', outputdir='./', store_time=True):
+
+    sep=sep.encode('utf-8').decode('unicode_escape')
+
+    start = time.time()
+
+    base = os.path.basename(filepath)
+    hostname = socket.gethostname().split(".")[0]
+
+    res = os.path.join(outputdir,f"{base}_{hostname}")
+
     if len(glob.glob(f"{base}_*"))>0:
-        return False
-    
-    print("Processing " + f)
-    data = pd.read_csv(f)
+        return '0\n'
+
+    print("Processing " + filepath)
+
+    data = pd.read_csv(filepath, sep=sep, engine='python')
     data_processed = is_land(data["decimalLatitude"],data["decimalLongitude"])
     data_processed["index"] = data["index"].values
+
     print(f"    Done | Saving as {res}")
-    data_processed.to_csv(res, index=False)
-    
-    print("--- %s seconds ---" % (time.time() - start_time))
-    
+    data_processed.to_csv(res, index=False, sep=sep, encoding='utf-8')
+
+    span = np.round((time.time() - start),2)
+    print("--- %s seconds ---" % span)
+    if store_time:
+        span = str(span)+'\n'
+        timefilepath = os.path.join(outputdir,f"time_{base}")
+        with open(timefilepath, 'a+', encoding='utf-8') as f:
+            f.write(span)
+
+    del data
+    del data_processed
+
+    return str(span)+'\n'
+
+
+def display_progress(inputdir, outputdir):
+
+    files2process = [file for file in os.listdir(inputdir)]
+    todo = len(files2process)
+
+    processedfiles = [file for file in os.listdir(outputdir) if 'time' not in file]
+    processedfiles = pd.DataFrame(np.array(processedfiles).reshape(-1,1), columns=["filename"])
+    processedfiles = processedfiles["filename"].str.split('_').str[:-1]
+    processedfiles = processedfiles.unique()
+    done = len(processedfiles)
+
+    print(f"{done}/{todo} ({np.round(done/todo,4)*100}%) files processed")
+
     return True
 
+
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Identify locations in the ocean')
+    parser.add_argument('inputdir', type=str, help='path to directory containing files to be processed')
+    parser.add_argument('--delimiter', type=str, help='file delimiter', default='\t')
+    parser.add_argument('--outputpath', type=str, help='path to folder where output files will be stored', default='./')
+    parser.add_argument('--store_time', action='store_true', help='store processing time')
+    args = parser.parse_args()
+
+    inputdir = args.inputdir
+    sep = args.delimiter
+    outputdir = os.path.join(args.outputpath,'processed')
+    store_time = args.store_time
+
     try:
-        os.mkdir("processed")
+        os.mkdir(outputdir)
     except:
         pass
-    #target_dir = _path + sys.argv[1]
-    target_dir = sys.argv[1]
-    
-    list_file = [os.path.join(target_dir,f) for f in os.listdir(target_dir)]
-    random.shuffle(list_file)
-    
+
+    fileslist = [os.path.join(inputdir,file) for file in os.listdir(inputdir)]
+    random.shuffle(fileslist)
+
     with Pool(16) as p:
-        lo = p.map(process_one_file,list_file)
+        lo = p.map(partial(process_one_file, sep=sep, outputdir=outputdir, store_time=store_time), fileslist)
+
+    if store_time:
+
+        files2process = [os.path.join(outputdir,file) for file in os.listdir(outputdir) if 'time' in file]
+
+        outputfile = os.path.join(outputdir,'time')
+
+        init=True
+        for filepath in files2process:
+
+            print(filepath)
+            content = pd.read_csv(filepath, names=["time"])
+            content["file"] = os.path.basename(filepath)
+            if init:
+                timefile = content.copy()
+                init=False
+            else:
+                timefile = pd.concat([timefile,content], ignore_index=True, axis=0)
+            print(timefile)
+            os.remove(filepath)
+
+        timefile.to_csv(outputfile, mode='w', index=False)
