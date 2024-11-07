83e8252513a3c913c7ed84df294140c084ebce96
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2024-11-07 20:58:12 +0100

    MODIFY regexstrip.py (allow asymmetric strip) | marinedb/utils

--- marinedb/utils/regexstrip.py
+++ marinedb/utils/regexstrip.py
@@ -5,7 +5,7 @@ def apply(string, pattern=None):
     if pattern is None:
         # By default, remove leading and trailing whitespace
         pattern = r'^\s+|\s+$'
-    else:
+    elif pattern[0]!='^':
         pattern = fr'^{pattern}|{pattern}$'
 
     return re.sub(fr'{pattern}','',string)
