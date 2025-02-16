aee6551f9b93287bbf3398ba79331e997a1bc052
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-02-16 12:53:38 +0100

    MODIFY _execute_java(), MODIFY & RENAME parse_java() & MODIFY apply() to align with gbif-date-parser-20250214.jar modifications ; CREATE _parse_javastdout() ; CREATE create_javaarg() ; CREATE validate_format() (temporary workaround for an issue in _execute_java()) ; CREATE gbif_parse_date() ; CREATE parallel_parse_date() in parsedate.py | marinedb/tools/temporal

--- marinedb/tools/temporal/parsedate.py
+++ marinedb/tools/temporal/parsedate.py
@@ -5,17 +5,20 @@ import subprocess
 import pandas as pd
 import os
 from joblib import Parallel, delayed
+import math
+import re
+import itertools
+
+import time #DEBUG
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-JAR_PATH = os.path.join(PATH,'gbif-date-parser-20250211.jar')
+JAR_PATH = os.path.join(PATH,'gbif-date-parser-20250214.jar')
 
-def execute_java(date_str, format=None):
+def _execute_java(multiple_datestr):
 
     # Execute the java command
 
-    cmd = ['java', '-jar', JAR_PATH, date_str]
-    if format is not None:
-        cmd.append(format)
+    cmd = ['java', '-jar', JAR_PATH, multiple_datestr]
 
     a = subprocess.Popen(
         cmd,
@@ -24,73 +27,139 @@ def execute_java(date_str, format=None):
         stderr=subprocess.PIPE,
     )
 
+    keys = []
+    stdout = []
+    for line in a.stdout:
+
+        line = line.decode("utf-8").strip()
+        nsep = line.count("=")
+        if nsep==1:
+            stdout.append(line)
+        elif nsep==2:
+            single_datestr = re.search(r'DATE=(.*)\s',line).group(1)
+            single_ordering = re.search(r'ORDERING=(.*)$',line).group(1)
+            keys.append((single_datestr,single_ordering))
+        else:
+            raise Exception(f'unexpected output: more than two equality signs in {line}')
+
+    stderr = []
+    for line in a.stderr:
+        line = line.decode("utf-8").strip()
+        stderr.append(line)
+
+    a.terminate()
+
+    return keys, stdout, stderr
+
+def _parse_javastdout(single_stdout, single_request):
+
+    datestr_processed = single_stdout.split("=")[1]
+
+    if len(datestr_processed)==0:
+        datestr_processed = pd.NA
+
+    else:
+        datestr_processed = datestr_processed.split()
+
+        if len(datestr_processed)!=2:
+            raise Exception(f'unexpected output for {single_request}: stdout={single_stdout}')
+
+        datestr_processed = sorted(list(set(datestr_processed)))
+        datestr_processed = '/'.join(datestr_processed)
+
+    return datestr_processed
+
+def parse_java(multiple_datestr):
+
+    # Execute the java command
+
+    keys, stdout, stderr = _execute_java(multiple_datestr)
+
     # Process the command output
     # format: SUCCESS:date1 date2 or ERROR:error or RAISE:error_message
 
     ## Capture raised errors
 
-    stderr = []
-    old_stderr = [] #DEBUG
-    for line in a.stderr:
-        line = line.decode('utf-8').strip()
-        old_stderr.aooend(line) #DEBUG
-        stderr_type = line.split('=')[0]
-        stderr_message = line.split('=')[1]
-        if stderr_type=='RAISE':
-            raise Exception(stderr_message)
-        stderr.append(stderr_message)
+    for i,single_stderr in enumerate(stderr):
 
-    if len(stderr)!=0:
-        if len(stderr)!=1:
-            print(old_stderr) #DEBUG
-            raise Exception(f'Unexpected output for date={date_str}: stderr={stderr}')
-        stderr = stderr[0]
-    else:
-        stderr = ''
+        single_stderr = single_stderr.split('=')
+
+        if len(single_stderr[1])!=0:
+
+            stderr_type = single_stderr[0]
+            stderr_message = single_stderr[1]
+            if stderr_type=='RAISE':
+                raise Exception(f'{keys[i]}: ' + stderr_message)
+            stderr[i]=stderr_message
+
+        else:
+            stderr[i]=pd.NA
 
     ## Extract the two parsed dates
 
-    stdout = []
-    old_stdout = [] #DEBUG
-    for line in a.stdout:
-        line = line.decode('utf-8').strip()
-        old_stdout.append(line) #DEBUG
-        stdout.append(line.split('=')[1])
+    for i,single_stdout in enumerate(stdout):
 
-    if len(stdout)!=0:
-        if len(stdout)!=1:
-            print(old_stdout) #DEBUG
-            raise Exception(f'Unexpected output for date={date_str}: stdout={stdout}')
-        stdout = stdout[0].split()
-        if len(stdout)!=2:
-            print(old_stdout) #DEBUG
-            raise Exception(f'Unexpected output for date={date_str}: stdout={stdout}')
+        stdout[i]=_parse_javastdout(single_stdout, keys[i])
 
-    a.terminate()
+    return list(zip(stdout,stderr))
 
-    if (len(stderr)==0) and (len(stdout)==0):
-        raise Exception(f'Unexpected output for date={date_str}: both stderr and stdout are empty')
-    #print(f'stdout: {stdout}, stderr : {stderr}')
+def validate_format(format): #TEMP ?
 
-    return stdout, stderr
+    _, _, stderr = _execute_java(f"('2003-06-02', '{format}')")
+    stderr = stderr[0].split('=')
+    if stderr[0] == 'RAISE':
+        raise Exception(stderr[1])
 
-def _parse_date(date_str, format=None):
+def create_javaarg(datesstr_list, format=None):
 
-    if pd.isnull(date_str):
-        return [pd.NA,pd.NA]
+    if format is None:
+        format=''
+    if isinstance(format,list):
+        if (len(datesstr_list)!=len(format)):
+            raise ValueError(f'`datesstr_list` and `format` must have the same length')
+        for fmt in format:
+            validate_format(fmt)
+    if isinstance(format,str):
+        validate_format(format)
+        format = [format]*len(datesstr_list)
+    if not isinstance(format,str | list):
+        raise TypeError(f'`format` must be a string or a list, not a {type(format).__name__}')
 
-    dates, issue = execute_java(date_str, format=format)
+    multiple_datestr = list(zip(datesstr_list,format))
+    multiple_datestr = [str(single_request) for single_request in multiple_datestr]
+    multiple_datestr = ';'.join(multiple_datestr)
 
-    if len(dates)==0:
-        return [pd.NA, issue]
-    else:
-        return ['/'.join(list(set(dates))),pd.NA] #PROBLÈME INTERVALLE INVERSÉ
+    return multiple_datestr
 
+def gbif_parse_date(df, key, index, format=None):
 
-def apply(df, key, format=None, inplace=False, cpu=None):
+    multiple_datesstr = df.loc[index,key].tolist()
+    multiple_datestr = create_javaarg(multiple_datesstr, format=format)
+    result = parse_java(multiple_datestr)
 
-    if cpu is None:
+    return result
+
+def parallel_parse_date(df, key, datestr_index, cpu, format=None):
+
+    ndates = len(datestr_index)
+    batch_size=1000
+    index_start = list(range(ndates))[::batch_size]
+    index_end = list(range(batch_size,ndates))[::batch_size] + [ndates]
+    index_slides = list(zip(index_start,index_end))
+
+    with Parallel(n_jobs=cpu, prefer='threads', verbose=10) as parallel:
+        results = parallel(delayed(gbif_parse_date)(df, key, datestr_index[start:end], format=format) for start,end in index_slides) # order of the outputs = order of submission
+
+    results = list(itertools.chain(*results))
+
+    return results
+
+def apply(df, key, format=None, inplace=False, parallel=False, cpu=None):
+
+    if parallel and cpu is None:
         cpu=len(os.sched_getaffinity(0))
+    if not parallel:
+        cpu=1
 
     print(f'        INFO | {cpu} CPUs will be used')
 
@@ -99,25 +168,42 @@ def apply(df, key, format=None, inplace=False, cpu=None):
     else:
         colname=f'{key}_processedby_parsedate'
 
-    df[key]=df[key].astype('string')
-    #dfByDates = df.loc[~pd.isnull(df[key]),[key]].groupby(key)
-    #unique_dates = list(dfByDates.groups.keys())
-    unique_dates = list(df.loc[~pd.isnull(df[key]),key].unique())
+    df[key] = df[key].astype('string')
+    isdate = (~pd.isnull(df[key]))
+    datestr_index = list(isdate[isdate].index)
 
-    ndates=len(unique_dates)
-    print(f'        * Standardizing date format | {ndates} unique dates')
-    with Parallel(n_jobs=cpu, prefer='threads', verbose=1) as parallel:
-        results = parallel(delayed(_parse_date)(date_str,format=format) for date_str in unique_dates) # order of the outputs = order of submission
-#        results = parallel(delayed(_parse_date)(df.loc[idx,key],format=format) for idx in range(Ndates))
-    print(results[:10])
-    #df[[colname,'issue_parsedate']] = results
+    time_start=time.time() #DEBUG
 
-    df[key] = df[key].fillna('_MISSING_')
-    df = df.set_index(key)
-    for idx,date in enumerate(unique_dates):
-        df.loc[date,[colname,'issue_parsedate']] = results[idx]
-    df = df.reset_index(drop=inplace).replace('_MISSING_',pd.NA)
+#    ndates = len(datestr2process)
+#    print(ndates) #DEBUG
+#    batch_size=1000
+#    nbatch = math.ceil(ndates/batch_size)
+#    print(nbatch) #DEBUG
+#    for batch in range(nbatch):
+
+#        start = batch*batch_size
+#        if batch==(nbatch-1):
+#            end = ndates
+#        else:
+#            end = start + batch_size
+
+#        index = datestr2process[start:end]
+#        multiple_datesstr = df.loc[index,key].tolist()
+#        multiple_datestr = create_javaarg(multiple_datesstr, format=format)
+
+#        df.loc[index,[colname,'issue_parsedate']] = parse_java(multiple_datestr)
+#        time_end=time.time() #DEBUG
+#        print(f'TIME: {round(time_end - time_start,0)}s for {end} lines')
+
+    df.loc[datestr_index,[colname,'issue_parsedate']] = parallel_parse_date(df, key, datestr_index, format=format, cpu=cpu)
+
+    condition = pd.isnull(df.loc[isdate,[colname,'issue_parsedate']]).all(axis=1)
+    if condition.any():
+        error_index = list(df[condition].index)
+        raise Exception(f'unexpected output: both stderr and stdout are empty for line(s) {error_index}')
+
     df['issue_parsedate'] = df['issue_parsedate'].astype('string')
+    df[colname] = df[colname].astype('string')
 
     return df
 
@@ -129,6 +215,6 @@ if __name__ == '__main__':
         sys.exit(1)
 
     if len(sys.argv) == 3:
-        parse_date(sys.argv[1], sys.argv[2])
+        parse_java(sys.argv[1], sys.argv[2])
     else:
-        parse_date(sys.argv[1])
+        parse_java(sys.argv[1])
