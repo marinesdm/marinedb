1d793b49ae6590ab4d3ab368286ebcbad948a7ca
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-02-17 00:31:46 +0100

    CREATE tqdmjoblib.py (progress bar for joblib) | marinedb/utils

---
+++ marinedb/utils/tqdmjoblib.py
@@ -0,0 +1,21 @@
+# from https://stackoverflow.com/questions/24983493/tracking-progress-of-joblib-parallel-execution
+
+import contextlib
+import joblib
+from tqdm import tqdm
+
+@contextlib.contextmanager
+def apply(tqdm_object):
+    """Context manager to patch joblib, enabling progress reporting in the provided tqdm progress bar"""
+    class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
+        def __call__(self, *args, **kwargs):
+            tqdm_object.update(n=self.batch_size)
+            return super().__call__(*args, **kwargs)
+
+    old_batch_callback = joblib.parallel.BatchCompletionCallBack
+    joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
+    try:
+        yield tqdm_object
+    finally:
+        joblib.parallel.BatchCompletionCallBack = old_batch_callback
+        tqdm_object.close()
