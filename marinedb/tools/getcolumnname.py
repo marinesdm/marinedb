3abe9b27ebde57088e20ecf906ce37ecfba038d4
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-03-11 14:27:18 +0100

    FIX (add isprocessed, add basekey, modify len(processedkey)>1 case) & MODIFY (raise an exception if already processed) apply() in getcolumnname.py | marinedb/tools

--- marinedb/tools/getcolumnname.py
+++ marinedb/tools/getcolumnname.py
@@ -1,39 +1,50 @@
 
-
-
 def apply(df, key, modulename, inplace):
 
     ismodulename = (len(modulename) != 0)
 
     if (not ismodulename) and (not inplace):
-        raise Exception(f'`getcolumnname.py` | inplace={inplace} but modulename={modulename}. Assign a value to `modulename` or set `inplace` to True.')
+        raise Exception(f"`getcolumnname.py` | inplace={inplace} but modulename='{modulename}'. Assign a value to `modulename` or set `inplace` to True.")
 
-    # Check if `key` has been previously processed and not modified in place,
-    # as indicated by the naming pattern '{key}_processedby_{modulename}_...'
+    # Check if `basekey` has been previously processed and not modified in place,
+    # as indicated by the naming pattern '{basekey}_processedby_{modulename}_...'
 
-    processedkey = [col for col in df.columns if (f'{key}_processedby' in col)]
+    basekey = key.split('_processedby_')[0]
+    processedkey = [col for col in df.columns if (f'{basekey}_processedby' in col)]
 
     if len(processedkey) > 1:
-        # unexpected
-        raise Exception(f"`getcolumnname.py` | Multiple processed columns found for '{key}': {processedkey}. An issue may have occurred during execution.")
+        processedkey = sorted(processedkey, key=len)
+        for col in processedkey:
+            coldiff = set(col) - set(processedkey[-1])
+            if (len(coldiff) != 0):
+                # unexpected
+                raise Exception(f"`getcolumnname.py` | Multiple processed columns found for '{basekey}': {processedkey}. An issue may have occurred during execution.")
+        processedkey = processedkey[-1:]
 
     if len(processedkey) == 1:
         # Previously processed and not modified in place
         inputkey = processedkey[0]
+        isprocessed = True
         isgenerated = False
 
     else:
         # Either unprocessed, modified in place or newly created
-        inputkey = key
-        isgenerated = (inputkey in df.columns)
+        inputkey = basekey
+        isprocessed = False
+        isgenerated = (inputkey not in df.columns)
 
     outputkey = inputkey
     if ismodulename:
         # Maintain a record of curation process
-        if isgenerated:
-            outputkey += f'_processedby_{modulename}'
+        if isprocessed:
+            if modulename in inputkey:
+                raise Exception(f"`getcolumnname.py` | '{basekey}' has already been processed by `{modulename}`: '{inputkey}'")
+            else:
+                outputkey += f'_{modulename}'
         else:
-            outputkey += f'_{modulename}'
+            outputkey += f'_processedby_{modulename}'
+            if isgenerated:
+                inputkey = outputkey
 
     if inplace and (outputkey != inputkey):
         # If `inplace` is True, overwrite the original column
