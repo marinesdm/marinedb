09ec41ac6e8e9f3e451165b0a2aa6b6230617371
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-10 21:52:31 +0200

    ADD shebang ; ADD imports (pandas, getcolumnname, doesnotcontain, allexport) ; ADD __all__ global variable & ADD @export where necessary ; MODIFY apply() (use doesnotcontain, add dropna, flagname_mapping & indent options) [major changes] ; in contains.py | marinedb/tools

--- marinedb/tools/contains.py
+++ marinedb/tools/contains.py
@@ -1,26 +1,67 @@
+#!/usr/bin/python
+# coding: utf-8
 
-def apply(df, key, values, flag=False):
+# External import
 
-    if isinstance(values, str):
-        values = [values]
+import pandas as pd
 
-    searchfor = ''.join([f'(?=.*{value})' for value in values])
-    keep = df[key].str.contains(rf'{searchfor}')
+# Internal import
+
+from marinedb.tools import getcolumnname
+from marinedb.tools import doesnotcontain
+from marinedb.utils.allexport import export
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+@export
+def apply(df, key, values, flag=False, flagname_mapping=None, dropna=False, indent=''):
+
+    df, key, _ = getcolumnname.apply(df, key, '', inplace=True)
+#    columns = set(df.columns)
+
+    # Run `doesnotcontain` on the `key` column
+
+    try:
+        df = doesnotcontain.apply(df, key, values, flag=True, flagname_mapping=flagname_mapping, indent=indent)
+    except ValueError as err:
+        raise ValueError(f"`contains.py` | {str(err).split('|')[-1]}")
+
+#    # Retrieve the `doesnotcontain` flag column
+#    doesnotcontain_flagcolumn = list(set(df.columns) - columns)
+#    assert len(doesnotcontain_flagcolumn) == 1
+#    doesnotcontain_flagcolumn = doesnotcontain_flagcolumn[0]
+
+    doesnotcontain_flagcolumn = [col in df.columns if (f'flag_{key}_doesnotcontain' in col)]
+    assert len(doesnotcontain_flagcolumn) == 1
+    doesnotcontain_flagcolumn = doesnotcontain_flagcolumn[0]
+    value_str = doesnotcontain_flagcolumn.split('_')[-1]
+
+    # Apply missing data handling strategy
+
+    ismissing = pd.isnull(df[doesnotcontain_flagcolumn])
+    df.loc[ismissing, doesnotcontain_flagcolumn] = dropna
+
+    doescontain = (~df[doesnotcontain_flagcolumn])
+
+    # Clean
+
+    df.drop(columns=doesnotcontain_flagcolumn, inplace=True)
 
     if flag:
 
-        if key=="issue":
-            with open('../gbif_issues.json','r') as issues_file:
-                issues_dict = json.load(issues_file)
-            value_str = '-'.join([str(issues_dict[key]["id"]) for key in values])
+        # Flag rows where the `key` column contains values in `values`
 
-        else:
-            value_str = '-'.join(values)
-
-        df[f'flag_{key}_contains_{value_str}'] = (~keep)
+        doescontain[ismissing] = pd.NA
+        df[f'flag_{key}_contains_{value_str}'] = doescontain
 
         return df
 
     else:
 
-        return df[keep].reset_index(drop=True)
+        # Drop rows:
+        #   - where `key` does not contain values in `values`
+        #   - with missing values in `key` if `dropna`
+
+        return df[doescontain].reset_index(drop=True)
