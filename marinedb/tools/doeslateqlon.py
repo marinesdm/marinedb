b237b1f786d228b8e628f53e6e630ebf47dc368b
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-10 21:57:12 +0200

    ADD shebang ; EDIT comments ; ADD imports (pandas, getcolumnname, allexport) ; ADD __all__ global variable & ADD @export where necessary ; MODIFY apply() (getcolumnname, add dropna option, handle missing data) ; in doeslateqlon.py | marinedb/tools

--- marinedb/tools/doeslateqlon.py
+++ marinedb/tools/doeslateqlon.py
@@ -1,16 +1,41 @@
-import numpy as np
+#!/usr/bin/python
+# coding: utf-8
 
-def apply(df, keylat, keylon, flag=False, eps=1e-5):
+# External import
 
-    #eps=1e-5:
-        #GBIF: coordinates rounded to 6 decimals
-        #5 decimals = ~1m precision at the equator (less elsewhere)
+import pandas as pd
+
+# Internal import
+
+from marinedb.utils.allexport import export
+from marinedb.tools import getcolumnname
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+@export
+def apply(df, keylat, keylon, flag=False, eps=1e-5, dropna=False):
+
+    # flag = True/False: whether to flag or drop observations that do not meet the condition
+    # default eps: 1e-5
+    #   - GBIF coordinates are rounded to six decimal places
+    #   - five decimals places correspond to a precision of 1 meter at the equator (i.e., higher precision elsewhere)
+
+    df, keylat, _ = getcolumnname.apply(df, keylat, '', inplace=True)
+    df, keylon, _ = getcolumnname.apply(df, keylon, '', inplace=True)
+
+    isequal = (df[keylat].astype('Float64') - df[keylon].astype('Float64')).abs() <= eps
+    ismissing = (pd.isnull(df[keylat]) | pd.isnull(df[keylon]))
+    isequal[ismissing] = pd.NA
 
     if flag:
-        df[f'flag_doeslateqlon'] = np.abs(df[keylat].astype('Float64') - df[keylon].astype('Float64')) <= eps
+        # Flag rows where latitude and longitude are equal
+        df[f'flag_{keylat}_{keylon}_doeslateqlon'] = isequal
         return df
-
     else:
-        return df[np.abs(df[keylat].astype('Float64') - df[keylon].astype('Float64')) > eps] #delete if lat or lon is NaN
-
-
+        # Drop rows:
+        #   - where latitude and longitude are equal
+        #   - with missing latitude and/or longitude when `dropna`
+        isequal[ismissing] = dropna
+        return df[~isequal]
