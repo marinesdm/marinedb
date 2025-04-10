1c6ef398acab4a431a6a484b1ed07c045b57fdeb
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-10 23:42:26 +0200

    ADD shebang ; EDIT comments ; ADD imports (pandas, getcolumnname, allexport, aligndtypes) ; ADD __all__ global variable & ADD @export where necessary ; MODIFY apply() (getcolumnname, aligndtypes, add flagname_mapping, dropna & indent options, handle missing data) ; in isin.py | marinedb/tools

--- marinedb/tools/isin.py
+++ marinedb/tools/isin.py
@@ -1,15 +1,73 @@
+#!/usr/bin/python
+# coding: utf-8
 
+# External import
 
-def apply(df, key, values, flag=False):
+import pandas as pd
 
-    if isinstance(values,str):
+# Internal import
+
+from marinedb.utils.allexport import export
+from marinedb.tools import getcolumnname
+from marinedb.tools import aligndtypes
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+@export
+def apply(df, key, values, flag=False, flagname_mapping=None, dropna=False, indent=''):
+
+    if (not flag) and (flagname_mapping is not None):
+        print(indent + f'INFO | `flagname_mapping` will be ignored (flag={flag})')
+        flagname_mapping = None
+
+    if (flagname_mapping is not None):
+        if not isinstance(flagname_mapping, str | dict):
+            raise ValueError('`isin.py` | `flagname_mapping` must be provided as either a dictionary or a JSON file path')
+        if isinstance(flagname_mapping, str):
+            try:
+                with open(flagname_mapping,'r') as file:
+                    flagname_mapping = json.load(file)
+            except ValueError:
+                raise ValueError('`isin.py` | if a string, `flagname_mapping` must be a path to a valid JSON file')
+
+    df, key, _ = getcolumnname.apply(df, key, '', inplace=True)
+
+    # Ensure that the objects being compared are of the same type
+
+    df, values = aligndtypes.apply(df, key, values)
+    if not isinstance(values, list):
         values = [values]
 
-    keep = df[key].isin(values)
+    # Filtering condition
+
+    condition = df[key].isin(values)
+    ismissing = pd.isnull(df[key])
+    condition[ismissing] = pd.NA
 
     if flag:
-        values_str = '-'.join(values)
-        df[f'flag_{key}_isin_{values_str}'] = (~keep)
+
+        # Flag rows where `key` values are in `values`
+
+        if flagname_mapping is not None:
+            temp = flagname_mapping.copy()
+            for k in flagname_mapping.keys():
+                temp[str(k)] = temp.pop(k)
+            flagname_mapping = temp
+            values_str = '-'.join([str(flagname_mapping[str(val)]) for val in values])
+        else:
+            values_str = '-'.join([str(val) for val in values])
+
+        df[f'flag_{key}_isin_{values_str}'] = condition
+
         return df
+
     else:
-        return df[keep].reset_index(drop=True)
+
+        # Drop rows where `key` values are NOT in `values`
+        # Note: rows with missing values in the `key` column are removed
+
+        condition[ismissing] = (not dropna)
+
+        return df[condition].reset_index(drop=True)
