b2d1002f65576df6c27e9fbcd931dfbee3de4b75
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-10 23:47:05 +0200

    ADD shebang ; EDIT comments ; ADD imports (pandas, getcolumnname, allexport) ; ADD __all__ global variable & ADD @export where necessary ; MODIFY apply() (getcolumnname, add dropna option, handle missing data) ; in iszero.py | marinedb/tools

--- marinedb/tools/iszero.py
+++ marinedb/tools/iszero.py
@@ -1,14 +1,40 @@
-import numpy as np
+#!/usr/bin/python
+# coding: utf-8
 
-def apply(df, key, flag=False, eps=1e-5):
+# External import
 
-    #eps=1e-5:
-        #GBIF: coordinates rounded to 6 decimals
-        #5 decimals = ~1m precision at the equator
+import pandas as pd
+
+# Internal import
+
+from marinedb.utils.allexport import export
+from marinedb.tools import getcolumnname
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+@export
+def apply(df, key, flag=False, eps=1e-5, dropna=False):
+
+    # flag = True/False: whether to flag or drop observations that do not meet the condition
+    # default eps: 1e-5
+    #   - GBIF coordinates are rounded to six decimal places
+    #   - five decimals places correspond to a precision of 1 meter at the equator (i.e., higher precision elsewhere)
+
+    df, key, _ = getcolumnname.apply(df, key, '', inplace=True)
+
+    iszero = (df[key].astype('Float64').abs() <= eps)
+    ismissing = pd.isnull(df[key])
+    iszero[ismissing] = pd.NA
 
     if flag:
-        df[f'flag_iszero_{key}'] = np.abs(df[key].astype('Float64')) <= eps
+        # Flag rows with null values in the `key` column
+        df[f'flag_{key}_iszero'] = iszero
         return df
-
     else:
-        return df[np.abs(df[key].astype('Float64')) > eps] #delete if NaN
+        # Drop rows:
+        #   - with null values in `key`
+        #   - with missing values in `key` if `dropna`
+        iszero[ismissing] = dropna
+        return df[~iszero]
