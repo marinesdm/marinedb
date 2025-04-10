29b990c4b7d568383f89b1c007eb71425875a2cd
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-10 23:07:46 +0200

    ADD shebang ; EDIT comments ; ADD imports (getcolumnname, allexport) ; ADD __all__ global variable & ADD @export where necessary ; MODIFY apply() (getcolumnname) ; in dropna.py | marinedb/tools

--- marinedb/tools/dropna.py
+++ marinedb/tools/dropna.py
@@ -1,9 +1,31 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# External import
+
 import pandas as pd
 
+# Internal import
+
+from marinedb.tools import getcolumnname
+from marinedb.utils.allexport import export
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+@export
 def apply(df, key, flag=False):
 
+    df, key, _ = getcolumnname.apply(df, key, '', inplace=True)
+
+    ismissing = pd.isnull(df[key])
+
     if flag:
-        df[f'flag_{key}_dropna'] = pd.isna(df[key])
+        # Flag rows with missing values in the `key` column
+        df[f'flag_{key}_dropna'] = ismissing
         return df
+
     else:
-        return df[~pd.isna(df[key])].reset_index(drop=True)
+        # Drop rows with missing values in the `key` column
+        return df[~ismissing].reset_index(drop=True)
