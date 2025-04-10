0d776c8ed9e72679ce473227efa11edbf0d96fa2
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-10 23:51:54 +0200

    ADD shebang ; EDIT comments ; ADD imports (pandas, getcolumnname, allexport) ; ADD __all__ global variable & ADD @export where necessary ; MODIFY apply() (getcolumnname, add dropna option, handle missing data, modify pattern) ; in lettersonly.py | marinedb/tools

--- marinedb/tools/lettersonly.py
+++ marinedb/tools/lettersonly.py
@@ -1,11 +1,38 @@
+#!/usr/bin/python
+# coding: utf-8
 
-def apply(df, key, flag=False):
+# External import
 
-    pattern=r'[0-9]'
-    delete = df[key].str.contains(pattern)
+import pandas as pd
+
+# Internal import
+
+from marinedb.utils.allexport import export
+from marinedb.tools import getcolumnname
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+@export
+def apply(df, key, flag=False, dropna=False):
+
+    df, key, _ = getcolumnname.apply(df, key, '', inplace=True)
+
+    df[key] = df[key].astype('string')
+
+    pattern=r'[^a-zA-Z]'
+    islettersonly = (~df[key].str.contains(pattern, na=False))
+    ismissing = pd.isnull(df[key])
+    islettersonly[ismissing] = pd.NA
 
     if flag:
-        df[f'flag_{key}_lettersonly'] = delete
+        # Flag rows where the `key` column contains only letters
+        df[f'flag_{key}_lettersonly'] = islettersonly
         return df
     else:
-        return df[~delete]
+        # Drop rows:
+        #   - where `key` contains non-letter characters
+        #   - with missing values in `key` if `dropna`
+        islettersonly[ismissing] = (not dropna)
+        return df[islettersonly]
