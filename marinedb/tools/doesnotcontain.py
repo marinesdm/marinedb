151efc1f0bb6c1e6de4b6f54d4c49931059e0591
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-10 23:00:45 +0200

    ADD shebang ; EDIT comments ; ADD imports (pandas, getcolumnname, allexport) ; ADD __all__ global variable & ADD @export where necessary ; MODIFY apply() (getcolumnname, add dropna, flagname_mapping & indent options, handle missing data) [major changes] ; in doesnotcontain.py | marinedb/tools

--- marinedb/tools/doesnotcontain.py
+++ marinedb/tools/doesnotcontain.py
@@ -1,27 +1,85 @@
-import json
+#!/usr/bin/python
+# coding: utf-8
 
-def apply(df, key, values, flag=False):
+# External import
+
+import json
+import pandas as pd
+
+# Internal import
+
+from marinedb.utils.allexport import export
+from marinedb.tools import getcolumnname
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+@export
+def apply(df, key, values, flag=False, flagname_mapping=None, indent='', dropna=False): #mappingfile=None):
+
+#    if (flagname_mapping is not None) and (mappingfile is not None):
+#        raise Exception('`doesnotcontain.py` | either `flagname_mapping` or `mappingfile` may be provided, but not both')
+
+    if (not flag) and (flagname_mapping is not None):
+        print(indent + f'INFO | `flagname_mapping` will be ignored (flag={flag})')
+        flagname_mapping = None
+
+    if (flagname_mapping is not None):
+        if not isinstance(flagname_mapping, str | dict):
+            raise ValueError('`doesnotcontain.py` | `flagname_mapping` must be provided as either a dictionary or a JSON file path')
+        if isinstance(flagname_mapping, str):
+            try:
+                with open(flagname_mapping,'r') as file:
+                    flagname_mapping = json.load(file)
+            except ValueError:
+                raise ValueError('`doesnotcontain.py` | if a string, `flagname_mapping` must be a path to a valid JSON file')
+
+    df, key, _ = getcolumnname.apply(df, key, '', inplace=True)
+
+    # Filtering conditions
 
     if isinstance(values, str):
         values = [values]
-
+    else:
+        values = [str(val) for val in values]
     searchfor = '|'.join(values)
-    delete = df[key].str.contains(rf'{searchfor}')
+
+    df['tempcol'] = df[key].copy()
+    df['tempcol'] = df['tempcol'].astype('string')
+
+    doesnotcontain = (~df['tempcol'].str.contains(rf'{searchfor}', regex=True, na=dropna))
+    ismissing = pd.isnull(df['tempcol'])
+
+    df.drop(columns='tempcol', inplace=True)
 
     if flag:
 
-        if key=="issue":
-            with open('filters/gbif_issues.json','r') as issues_file:
-                issues_dict = json.load(issues_file)
-            value_str = '-'.join([str(issues_dict[key]["id"]) for key in values])
+        # Flag rows where the `key` column DOES NOT contain values in `values`
 
+#        if (mappingfile is not None) or (flagname_mapping is not None):
+#            if mappingfile is not None:
+#                with open(mappingfile,'r') as file:
+#                    rename_values = json.load(file)
+
+        if flagname_mapping is not None:
+            temp = flagname_mapping.copy()
+            for k in flagname_mapping.keys():
+                temp[str(k)] = temp.pop(k)
+            flagname_mapping = temp
+            value_str = '-'.join([str(flagname_mapping[str(val)]) for val in values])
         else:
             value_str = '-'.join(values)
 
-        df[f'flag_{key}_doesnotcontain_{value_str}'] = delete
+        doesnotcontain[ismissing] = pd.NA
+        df[f'flag_{key}_doesnotcontain_{value_str}'] = doesnotcontain
 
         return df
 
     else:
 
-        return df[~delete].reset_index(drop=True)
+        # Drop rows:
+        #   - where `key` contains values in `values`
+        #   - with missing values in `key` if `dropna`
+
+        return df[doesnotcontain].reset_index(drop=True)
