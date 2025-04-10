304c93eff7c402bd3466766ffd59ae8265a2b37a
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-11 00:11:35 +0200

    ADD shebang ; EDIT comments & print statements ; CLEAN imports (remove numpy) ; ADD imports (pandas, getcolumnname, allexport) ; ADD __all__ global variable & ADD @export where necessary ; MODIFY apply() (getcolumnname, add dropna & indent options, handle missing data, modify flag_columns, precision_columns & isbelow_minlatlonprecision) ; in minlatlonprecision.py | marinedb/tools

--- marinedb/tools/minlatlonprecision.py
+++ marinedb/tools/minlatlonprecision.py
@@ -1,29 +1,73 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# External import
+
+import pandas as pd
+
+# Internal import
+
+from marinedb.tools import getcolumnname
+from marinedb.utils.allexport import export
 from marinedb.tools import minfloatprecision as mfp
-import numpy as np
 
-def apply(df, keylat, keylon, value, flag=False):
+# Global variable
 
-    print(f'            * minlatlonprecision | minfloatprecision for {keylat}')
-    df = mfp.apply(df, keylat, value, flag=True)
-    print(f'            * minlatlonprecision | minfloatprecision for {keylon}')
-    df = mfp.apply(df, keylon, value, flag=True)
-    flagcolumns = [f'flag_{keylat}_minfloatprecision_{str(value)}', f'flag_{keylon}_minfloatprecision_{str(value)}']
-    precisioncolumns = [f'{keylat}_precision',f'{keylon}_precision']
+__all__ = [] # populated using the @export decorato
 
-    doescontain_necessarycolumns = set(flagcolumns+precisioncolumns) - set(df.columns)
-    if len(doescontain_necessarycolumns)!=0:
-        raise Exception(f'Running minfloatprecision for latitude and longitude should produce 4 columns.')
+@export
+def apply(df, keylat, keylon, value, flag=False, dropna=False, indent=''):
 
-    flagname = f'flag_minlatlonprecision_{str(value)}'
-    df[flagname] = df[flagcolumns[0]] * df[flagcolumns[1]] #latitude AND longitude below a threshold of `value` decimals
+    df, keylat, _ = getcolumnname.apply(df, keylat, '', inplace=True)
+    df, keylon, _ = getcolumnname.apply(df, keylon, '', inplace=True)
 
-    dropcolumns = flagcolumns + precisioncolumns
+    # Apply `minfloatprecision` separately to latitude and longitude
+
+    print(indent + f'* minlatlonprecision | minfloatprecision for {keylat}')
+    df = mfp.apply(df, keylat, value, flag=True, dropna=dropna, indent=(indent + '  '))
+    print(indent + f'* minlatlonprecision | minfloatprecision for {keylon}')
+    df = mfp.apply(df, keylon, value, flag=True, dropna=dropna, indent=(indent + '  '))
+
+    columns = list(df.columns)
+    flag_columns = [col for col in columns if ('flag' in col) and ('minfloatprecision' in col)]
+    assert len(flag_columns) == 2
+    precision_columns = [col for col in columns if ('generatedby' in col) and ('minfloatprecision' in col)]
+    assert len(precision_columns) == 2
+    dropcolumns = flag_columns + precision_columns
+
+    # Check whether the float precision of latitude and longitude is below `value`
+
+    isbelow_minlatlonprecision = (df[flag_columns[0]] & df[flag_columns[1]])
+    ismissing = (pd.isnull(df[keylat]) | pd.isnull(df[keylon]))
+    isbelow_minlatlonprecision[ismissing] = pd.NA
 
     if flag:
-        df['latlon_precision'] = df[precisioncolumns].max(axis=1).astype('Int64')
+
+        # Flag rows where latitude and longitude precision falls below `value` decimal places
+
+        ## Precision
+        precision = df[precision_columns].max(axis=1).astype('Int64')
+        precision[ismissing] = pd.NA
+        df[f'{keylat}_{keylon}_floatprecision_generatedby_minlatlonprecision'] = precision
+
+        ## Flag
+        df[f'flag_{keylat}_{keylon}_minlatlonprecision_{str(value)}'] = isbelow_minlatlonprecision
+
+        ## Clean
         df.drop(columns=dropcolumns, inplace=True)
+
         return df
+
     else:
-        df = df[~df[flagname]].reset_index(drop=True)
-        df.drop(columns=dropcolumns+[flagname], inplace=True)
-        return df
+
+        # Drop rows:
+        #   - where latitude and longitude precision falls below `value` decimal places
+        #   - with missing latitude and/or longitude when `dropna`
+
+        ## Apply missing data handling strategy
+        isbelow_minlatlonprecision[ismissing] = dropna
+
+        ## Clean
+        df.drop(columns=dropcolumns, inplace=True)
+
+        return df[~isbelow_minlatlonprecision].reset_index(drop=True)
