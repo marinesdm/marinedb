8ef955da9754f0930d97fa7b09f7adab93b13d85
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-11 11:20:39 +0200

    ADD shebang ; EDIT comments ; ADD import (allexport) ; ADD __all__ global variable & ADD @export where necessary ; in isgzip.py [minor changes] | marinedb/utils

--- marinedb/utils/isgzip.py
+++ marinedb/utils/isgzip.py
@@ -1,4 +1,17 @@
+#!/usr/bin/python
+# coding: utf-8
 
+# from: https://stackoverflow.com/questions/3703276/how-to-tell-if-a-file-is-gzip-compressed
+
+# Internal import
+
+from marinedb.utils.allexport import export
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+@export
 def apply(filepath):
     with open(filepath, 'rb') as test:
         return (test.read(2) == b'\x1f\x8b')
