a9750032d51166cb744b679e4eb5f469b86c7e66
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-11 10:48:06 +0200

    ADD shebang ; EDIT comments & exceptions ; CLEAN imports (remove operator) ; ADD imports (os, allexport) ; ADD __all__ global variable & ADD @export where necessary ; MODIFY apply() (modify rankfile_path) ; in subsetranks.py | marinedb/tools

--- marinedb/tools/subsetranks.py
+++ marinedb/tools/subsetranks.py
@@ -1,41 +1,66 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# External import
+
+import os
 import yaml
 import pathlib
-from operator import itemgetter
 
-def _add_alternativeranks(ranks, eqranks):
+# Internal import
+
+from marinedb.utils.allexport import export
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+def add_alternativeranks(ranks, eqranks):
+
     alternative_ranks = list(eqranks.keys())
     add = [r for r in alternative_ranks if eqranks[r] in ranks]
+
     return ranks + add
 
+@export
 def apply(identification_level, lower=False, strict=True):
 
-    rankfile_path = str(pathlib.Path(__file__).parent.resolve()) + '/taxonomicRanks.yaml'
+    # Load `taxonomicRanks.yaml` file
+
+    rankfile_path = os.path.join(pathlib.Path(__file__).parent.resolve(), 'taxonomicRanks.yaml')
 
     with open(rankfile_path,'r') as file:
-        rankfile=yaml.safe_load(file)
-        ranks=rankfile['ascending_ranks']
-        eqranks=rankfile['equivalent_ranks']
+        rankfile = yaml.safe_load(file)
+        ranks = rankfile['ascending_ranks']
+        eqranks = rankfile['equivalent_ranks']
 
-    identification_level=identification_level.lower()
+    identification_level = identification_level.lower()
+
+    # Retrieve all taxonomic ranks either below or above `identification_level`, depending on `lower`
+    # Include the rank itself unless `strict` is True
 
     try:
-        index=ranks.index(identification_level)
+        index = ranks.index(identification_level)
     except ValueError:
         if identification_level not in eqranks.keys():
-            raise ValueError(f'{identification_level} is an unknown identification level. See `taxonomic_ranks.yaml`.')
+            raise ValueError(f"`subsetranks.py` | identification_level='{identification_level}' is not a recognized taxonomic rank. See `taxonomic_ranks.yaml` for accepted values.")
         else:
-            identification_level=eqranks[identification_level]
-            index=ranks.index(identification_level)
+            identification_level = eqranks[identification_level]
+            index = ranks.index(identification_level)
 
     if lower:
         if strict:
-            ranks=ranks[:index]
+            ranks = ranks[:index]
         else:
-            ranks=ranks[:index+1]
+            ranks = ranks[:index+1]
     else:
         if strict:
-            ranks=ranks[index+1:]
+            ranks = ranks[index+1:]
         else:
-            ranks=ranks[index:]
+            ranks = ranks[index:]
 
-    return _add_alternativeranks(ranks, eqranks)
+    # Add equivalent rank names to handle alternate naming conventions
+
+    ranks = add_alternativeranks(ranks, eqranks)
+
+    return ranks
