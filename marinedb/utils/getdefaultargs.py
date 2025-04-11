93777caf4402831612ec92eb7eb0d32875d978a4
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-11 11:18:56 +0200

    ADD shebang ; EDIT comments ; ADD import (allexport) ; ADD __all__ global variable & ADD @export where necessary ; in getdefaultargs.py [minor changes] | marinedb/utils

--- marinedb/utils/getdefaultargs.py
+++ marinedb/utils/getdefaultargs.py
@@ -1,5 +1,21 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# from: https://stackoverflow.com/questions/12627118/get-a-function-arguments-default-value
+
+# External import
+
 import inspect
 
+# Internal import
+
+from marinedb.utils.allexport import export
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+@export
 def apply(func):
 
     signature = inspect.signature(func)
