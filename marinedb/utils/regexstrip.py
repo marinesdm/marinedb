fead8199416c6da62b4ca618a75a924d15581458
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-11 11:26:56 +0200

    ADD shebang ; EDIT comments ; ADD import (allexport) ; ADD __all__ global variable & ADD @export where necessary ; in regexstrip.py [minor changes] | marinedb/utils

--- marinedb/utils/regexstrip.py
+++ marinedb/utils/regexstrip.py
@@ -1,11 +1,25 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# External import
+
 import re
 
+# Internal import
+
+from marinedb.utils.allexport import export
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+@export
 def apply(string, pattern=None):
 
     if pattern is None:
-        # By default, remove leading and trailing whitespace
+        # by default, remove leading and trailing whitespace
         pattern = r'^\s+|\s+$'
-    elif pattern[0]!='^':
+    elif pattern[0] != '^':
         pattern = fr'^{pattern}|{pattern}$'
 
     return re.sub(fr'{pattern}','',string)
