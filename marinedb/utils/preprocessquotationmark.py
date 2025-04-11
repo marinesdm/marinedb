3c11ff6b5b261b31ae098bc9eb14a7adcfba5f2c
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-11 11:23:16 +0200

    ADD shebang ; EDIT comments & exceptions ; ADD import (allexport) ; ADD __all__ global variable & ADD @export where necessary ; in preprocessquotationmark.py [minor changes] | marinedb/utils

--- marinedb/utils/preprocessquotationmark.py
+++ marinedb/utils/preprocessquotationmark.py
@@ -1,6 +1,20 @@
-import pandas as pd
-from marinedb.utils import regexstrip
+#!/usr/bin/python
+# coding: utf-8
 
+# External import
+
+import pandas as pd
+
+# Internal import
+
+from marinedb.utils import regexstrip
+from marinedb.utils.allexport import export
+
+# Global variable
+
+__all__ = [] # populated using the @export decorator
+
+@export
 def apply(target):
 
     # Pre-process strings to prevent quotation mark issues in pandas
@@ -10,12 +24,12 @@ def apply(target):
         target = regexstrip.apply(target, pattern=r'["\s]+')
         target = regexstrip.apply(target, pattern=r"['\s]+")
 
-    elif isinstance(target, list | tuple | pd.Series): #python >= 3.10
+    elif isinstance(target, list | tuple | pd.Series): # python >= 3.10
 
-        target=pd.Series(target).str.replace('^["\s]+|["\s]+$','',regex=True)
-        target=target.str.replace("^['\s]+|['\s]+$",'',regex=True).tolist()
+        target = pd.Series(target).str.replace('^["\s]+|["\s]+$','',regex=True)
+        target = target.str.replace("^['\s]+|['\s]+$",'',regex=True).tolist()
 
     else:
-        raise ValueError(f'Type not recognized: {type(target)}')
+        raise ValueError(f'`preprocessquotationmark.py` | Type not recognized: {type(target).__name__}')
 
     return target
