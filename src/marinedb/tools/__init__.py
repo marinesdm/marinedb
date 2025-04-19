b931adf55b2fb76ec4c9fe76792bd92dc5f7dd66
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-19 15:37:42 +0200

    ADD shebang ; EDIT comments ; MODIFY imports (add os, pandas, spatial, taxonomic, marineloc) ; add __all__ global variable ; MODIFY apply() (add indent, store_stats & outputfile_stats options) ; in __init__.py (NOT TESTED) | marinedb/tools

--- src/marinedb/tools/__init__.py
+++ src/marinedb/tools/__init__.py
@@ -1,10 +1,25 @@
+#!/usr/bin/python
+# coding: utf-8
 
-# Local imports
+# External imports
+
+import os
+import pandas as pd
+
+# Internal imports
 
 from . import *
+from .spatial import *
 from .temporal import *
+from .taxonomic import *
+from .marineloc import *
 
-def apply(df, config_dict):
+__all__ = ['contains', 'doesnotcontain','dropvalues','isboundedby','isin','isna','notisin','marineloc','doeslateqlon','isbelow_minlatlonprecision','iszero','lettersonly','taxasubset','parsedate','processdateinterval','splitdate','temporal']
+
+def apply(df, config_dict, indent=indent, store_stats=True, outputfile_stats='cleaning_stats.txt'):
+
+    header = []
+    stats = []
 
     for procstep in config_dict:
 
@@ -23,24 +38,48 @@ def apply(df, config_dict):
 
             length_before=len(df)
 
-            if colname=='tool':
-                colname = [key for key in proc_params.keys() if 'key' in key]
-                if len(colname)!=0:
-                    print(f'    * {", ".join(colname)}')
-                else:
-                    print(f'    * dataframe')
-                #print(f'      ** {proc_name}')
-                df = eval(f"{proc_name}.apply(df, **proc_params)")
-            else:
-                print(f'    * {colname}')
-                #print(f'      ** {proc_name}')
-                df = eval(f"{proc_name}.apply(df, colname, **proc_params)")
+            if colname == 'tool':
 
-            print(f'    {proc_name} | before: {length_before}, after: {len(df)}')
+                colname = [key for key in proc_params.keys() if 'key' in key]
+                if len(colname) != 0:
+                    print(indent + f'* {", ".join(colname)}')
+                    procstep_string = f'{"-".join(colname)}'
+                else:
+                    print(indent + f'* dataframe')
+                    procstep_string = 'dataframe'
+                print(indent + f'** {proc_name}')
+                procstep_string = '_'.join(procstep_string, proc_name)
+
+                proc_params['indent'] = indent + '   '
+                df = eval(f"{proc_name}.apply(df, indent='   ', **proc_params)")
+
+            else:
+
+                print(indent + f'* {colname}')
+                print(indent + f'** {proc_name}')
+                proc_params['indent'] = indent + '   '
+
+                df = eval(f"{proc_name}.apply(df, colname, indent='   ', **proc_params)")
+
+            print()
+            length_after = len(df)
+            print(indent + f'{proc_name} | before: {length_before}, after: {length_after}')
+            if store_stats:
+                header += [f'{procstep_string}' + '_before', f'{procstep_string}' + '_after']
+                stats += [length_before, length_after]
 
             columns_after = set(df.columns)
             new_columns = columns_after - columns_before
-            if len(new_columns)!=0:
+            if len(new_columns) != 0:
                 print(f'    {proc_name} | new columns: {list(new_columns)}')
+            print()
+
+    if store_stats:
+        stats = pd.DataFrame([stats], columns=header, dtype=int)
+        if os.path.exists(outputfile_stats):
+            stats.to_csv(outputfile_stats, sep='\t', index=False, header=False, mode='a')
+        else:
+            stats.to_csv(outputfile_stats, sep='\t', index=False)
 
     return df
+
