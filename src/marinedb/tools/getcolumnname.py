1f1d2cb7212a9ebd7105b502c567ea33185cbc49
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-19 15:29:27 +0200

    MODIFY apply() (add minimize_columns option, modify outputkey if isgenerated) in getcolumnname.py (NOT TESTED) | marinedb/tools

--- src/marinedb/tools/getcolumnname.py
+++ src/marinedb/tools/getcolumnname.py
@@ -10,7 +10,16 @@ from marinedb.utils.allexport import export
 __all__ = [] # populated using the @export decorator
 
 @export
-def apply(df, key, modulename, inplace):
+def apply(df, key, modulename, inplace, minimize_columns=True):
+
+    """
+    Parameters
+    ----------
+    minimize_columns : bool
+        If True, minimizes the number of generated columns by modifying derived columns inplace
+        If False, a new column may be created for each processing step (i.e., when inplace=False),
+        allowing fine-grained tracking of the data curation process
+    """
 
     ismodulename = (len(modulename) != 0)
 
@@ -25,24 +34,36 @@ def apply(df, key, modulename, inplace):
 
     if len(processedkey) > 1:
         processedkey = sorted(processedkey, key=len)
-        for col in processedkey:
-            coldiff = set(col) - set(processedkey[-1])
-            if (len(coldiff) != 0):
-                # unexpected
-                raise Exception(f"`getcolumnname.py` | Multiple independently processed columns found for '{basekey}': {processedkey}. An issue may have occurred during execution.")
-        processedkey = processedkey[-1:]
+        if minimize_columns:
+            raise Exception(f"`getcolumnname.py` | Multiple derived columns found for '{basekey}': {processedkey}.  An issue may have occurred during execution.")
+        else:
+            for col in processedkey:
+                coldiff = set(col) - set(processedkey[-1])
+                if (len(coldiff) != 0):
+                    # unexpected
+                    raise Exception(f"`getcolumnname.py` | Multiple independently processed columns found for '{basekey}': {processedkey}. An issue may have occurred during execution.")
+            processedkey = processedkey[-1:]
 
     if len(processedkey) == 1:
 
-        # Previously processed and not modified in place
+        # Previously processed
 
         inputkey = processedkey[0]
         isprocessed = True
         isgenerated = False
 
+        if minimize_columns:
+            # To prevent the proliferation of columns resulting from
+            # inplace=False settings in various processing steps, stop
+            # generating new columns after the first derived column is created
+            inplace = True
+
     else:
 
-        # Either unprocessed, modified in place or newly created
+        # Either:
+        # - unprocessed,
+        # - modified in place with the column name left unchanged
+        # - or newly created
 
         inputkey = basekey
         isprocessed = False
@@ -59,9 +80,11 @@ def apply(df, key, modulename, inplace):
             else:
                 outputkey += f'_{modulename}'
         else:
-            outputkey += f'_processedby_{modulename}'
             if isgenerated:
+                outputkey += f'_generatedby_{modulename}'
                 inputkey = outputkey
+            else:
+                outputkey += f'_processedby_{modulename}'
 
     if inplace and (outputkey != inputkey):
 
