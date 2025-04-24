32fe4161b6dce4accc100097c211b01c35990399
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-24 23:42:07 +0200

    FIX (convert isequal to boolean dtype) & MODIFY (rename latkey & lonkey) apply() in doeslateqlon.py | marinedb/tools/spatial

--- src/marinedb/tools/spatial/doeslateqlon.py
+++ src/marinedb/tools/spatial/doeslateqlon.py
@@ -16,23 +16,24 @@ from marinedb.tools import getcolumnname
 __all__ = [] # populated using the @export decorator
 
 @export
-def apply(df, keylat, keylon, flag=False, eps=1e-5, dropna=False, indent=''):
+def apply(df, latkey, lonkey, flag=False, eps=1e-5, dropna=False, indent=''):
 
     # flag = True/False: whether to flag or drop observations that do not meet the condition
     # default eps: 1e-5
     #   - GBIF coordinates are rounded to six decimal places
     #   - five decimals places correspond to a precision of 1 meter at the equator (i.e., higher precision elsewhere)
 
-    df, keylat, _ = getcolumnname.apply(df, keylat, '', inplace=True)
-    df, keylon, _ = getcolumnname.apply(df, keylon, '', inplace=True)
+    df, latkey, _ = getcolumnname.apply(df, latkey, '', inplace=True)
+    df, lonkey, _ = getcolumnname.apply(df, lonkey, '', inplace=True)
 
-    isequal = (df[keylat].astype('Float64') - df[keylon].astype('Float64')).abs() <= eps
-    ismissing = (pd.isnull(df[keylat]) | pd.isnull(df[keylon]))
+    isequal = (df[latkey].astype('Float64') - df[lonkey].astype('Float64')).abs() <= eps
+    isequal = isequal.astype('boolean')
+    ismissing = (pd.isnull(df[latkey]) | pd.isnull(df[lonkey]))
     isequal[ismissing] = pd.NA
 
     if flag:
         # Flag rows where latitude and longitude are equal
-        df[f'flag_{keylat}_{keylon}_doeslateqlon'] = isequal
+        df[f'flag_{latkey}_{lonkey}_doeslateqlon'] = isequal
         return df
     else:
         # Drop rows:
