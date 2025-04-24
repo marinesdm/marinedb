ea9486d17e3e3a2f39e4831989ebc96275ed1ebf
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-25 01:41:56 +0200

    EDIT print statements ; FIX & ADD imports (os, math, inspect, expanduser, marineloc, preprocessquotationmark) ; ADD printv import & MODIFY functions accordingly ; FIX get_dtypes() (replace column with coldict) ; FIX (add config_updated & base_colmapping, modify config_variables, replace column with coldict, modify indents, return config_updated) & MODIFY (add new name & dtypes to addcolumns columns) update_config() ; FIX (modify indents) & MODIFY (add verbose option, add isprint) dtypeconversion() ; RENAME & MODIFY (add config_updated & verbose options, add initial dtypes conversion, create, use & return config_updated) curate_data() ; FIX (str(config), preprocessquotationmark, force column order before storing) & MODIFY (add validation steps, rename outputdir, config_updated) __main__() ; in clean.py | .

--- src/marinedb/clean.py
+++ src/marinedb/clean.py
@@ -3,25 +3,32 @@
 
 # External imports
 
+import os
 import re
 import gzip
 import yaml
 import time
 import copy
+import math
+import inspect
 import argparse
 import numpy as np
 import pandas as pd
+from os.path import expanduser
 
 # Internal import
 
 import tools
 from marinedb.tools import getcolumnname
 from marinedb.tools import convertdatetype
-from marinedb.tools import createwormsfilters as cwf
+from marinedb.tools.marineloc import marineloc
+from marinedb.tools.taxonomic import createwormsfilters as cwf
 
 from marinedb.utils import readfile
 from marinedb.utils import writedataframe
 from marinedb.utils import standardizenan
+from marinedb.utils import preprocessquotationmark
+from marinedb.utils.printverbose import printv
 
 # Global variable
 
@@ -36,6 +43,10 @@ TYPE = {
 
 BATCH_SIZE = 100000 #issues if too big
 
+#def printv(message, verbose, indent=''):
+#    if verbose:
+#        print(indent + message)
+#    return True
 
 def get_key(onekeydict):
     if isinstance(onekeydict, str):
@@ -63,9 +74,9 @@ def get_dtypes(config):
 
         if isinstance(coldict,dict):
             colname_old = get_key(coldict)
-            if isinstance(column[colname_old], dict):
-                colname_new = get_key(column[colname_old])
-                coltype = column[colname_old][colname_new]
+            if isinstance(coldict[colname_old], dict):
+                colname_new = get_key(coldict[colname_old])
+                coltype = coldict[colname_old][colname_new]
                 if (coltype in TYPE.keys()):
                     coltype = TYPE[coltype]
                 dtypes_mapping[colname_old] = coltype
@@ -73,20 +84,23 @@ def get_dtypes(config):
     return dtypes_mapping
 
 
-def update_config(config, columns, addcolumns=None):
+def update_config(df, config, addcolumns=None):
+#    print('config variables before:')
+#    print(config['variables']) #debug
 
-    config_variables = copy.deepcopy(config['variables'])
+    config_updated = copy.deepcopy(config)
 
-    for idx, coldict in enumerate(config_variables):
-
+    config_variables = []
+    base_colmapping = {}
+    for idx, coldict in enumerate(config['variables']):
+        print('coldict:', coldict)
         add = None
-
         colname_old = get_key(coldict)
 
         # Retrieve column names post-processing
 
         _, colname_proc, _ = getcolumnname.apply(df, colname_old, '', inplace=True)
-
+        print(colname_proc)
         if ('processedby' in colname_proc):
 
             # The column has been modified, with modifications
@@ -98,7 +112,7 @@ def update_config(config, columns, addcolumns=None):
 
                 # Map the derived column to its intended name after renaming
 
-                colname_new = get_key(column[colname_old])
+                colname_new = get_key(coldict[colname_old])
                 colname_proc_new = re.sub(colname_old, colname_new, colname_proc)
 
                 add = {colname_proc: colname_proc_new}
@@ -111,32 +125,49 @@ def update_config(config, columns, addcolumns=None):
 
                     add = {colname_proc: {colname_proc_new: colname_proc_dtype}}
 
-            if (colname_old not in columns):
+        if (colname_old in list(df.columns)):
 
-                # The column has been modified in place during processing:
-                # Update the `variables` section in `config` to remove
-                # the settings for the original column
+            # The column has not been modified in place during processing:
+            # Update the `variables` section in `config` to include
+            # the settings for the original column
 
-                del config['variables'][idx]
+            config_variables.append(coldict)
+
+#           del config['variables'][idx]
 
         if add is not None:
 
             # Update the `variables`section in `config` to include
             # the settings for the post-processing column
 
-            config['variables'].append(add)
+            config_variables.append(add)
+
+        if isinstance(coldict, dict):
+            colname_new = get_key(coldict[colname_old])
+            base_colmapping.update({colname_old: colname_new})
 
     if addcolumns is not None:
 
         # Add the columns generated during processing
         # to the list of columns to keep
 
-        selected_columns = get_keys(config['variables'])
+        selected_columns = get_keys(config_variables)
         for col in addcolumns:
             if col not in selected_columns:
-                config['variables'].append(col)
+                dtype = str(df[col].dtype)
+                dtype = (dtype if (dtype != 'object') else 'string')
+                add = {col:{col:dtype}}
+                for colname_old, colname_new in base_colmapping.items():
+                    if colname_old in col:
+                        col_new = re.sub(colname_old, colname_new, col)
+                        add = {col:{col_new:dtype}}
+                        break
+                config_variables.append(add)
 
-    return config
+    config_updated['variables'] = config_variables
+    print('config variables after:')
+    print(config_updated['variables']) #debug
+    return config_updated
 
 
 def rename_columns(config):
@@ -157,14 +188,16 @@ def rename_columns(config):
     return colnames_mapping
 
 
-def dtypeconversion(df, config, indent=''):
+def dtypeconversion(df, config, verbose=True, indent=''):
+
+    isprint = False
 
     config_variables = config['variables']
 
     for column in config_variables:
 
+        colname_old = get_key(column)
         if isinstance(column, dict):
-            colname_old = get_key(column)
             if isinstance(column[colname_old], dict):
                 colname_new = get_key(column[colname_old])
                 coltype = column[colname_old][colname_new]
@@ -182,47 +215,69 @@ def dtypeconversion(df, config, indent=''):
 
                 try:
                     if 'datetime' in coltype:
+                        printv(f"WARNING | When converting '{colname_old}' to datetime, missing days and months will default to the 1st and January", verbose=verbose, indent=indent)
+                        isprint = True
                         df = convertdatetype.apply(df, datekey=colname_old, format='ISO8601')
-
                     if known_key:
                         df[colname_old] = df[colname_old].astype(TYPE[coltype])
                     else:
                         df[colname_old] = df[colname_old].astype(coltype)
                 except (TypeError, ValueError):
-                    print(indent + f"WARNING | Failed to convert '{colname_old}' to `{coltype}`")
+                    printv(f"WARNING | Failed to convert '{colname_old}' to `{coltype}`", verbose=verbose, indent=indent)
                     coltype = ''
+                    isprint = True
+#                    df[colname_old] = df[colname_old].astype(TYPE[coltype]) #debug
 
             else:
 
-                print(indent + f"INFO | '{colname_old}': `{coltype}` is not a recognized type")
+                printv(f"INFO | '{colname_old}': `{coltype}` is not a recognized type", verbose=verbose, indent=indent)
+                isprint = True
                 try:
                     df[colname_old] = df[colname_old].astype(coltype)
                 except (TypeError, ValueError):
-                    print(indent + f"WARNING | Failed to convert '{colname_old}' to `{coltype}`")
+                    printv(f"WARNING | Failed to convert '{colname_old}' to `{coltype}`", verbose=verbose, indent=indent)
+                    isprint = True
                     coltype = ''
 
         if (coltype == ''):
 
-            print(indent + f"INFO | Convert '{colname_old}' to `str` by default")
+            printv(f"INFO | Convert '{colname_old}' to `str` by default", verbose=verbose, indent=indent)
+            isprint = True
             df[colname_old] = df[colname_old].astype('string')
+
+    if isprint:
+        printv('', verbose=verbose)
 
     return df
 
 
-def processing_data(df, config, init=False, indent=''):
+def curate_data(df, config, config_updated, init=False, verbose=True, indent=''):
 
     # Standardize the missing values
 
-    print(indent + f'* dataframe')
-    print(indent + f'** standardizenan')
-    print()
+    printv(f'* dataframe', verbose=verbose, indent=indent)
+    printv(f'** standardizenan', verbose=verbose, indent=indent)
+    printv('', verbose=verbose, indent=indent)
     df = standardizenan.apply(df, key=None, letters_only=False)
 
     columns_before = set(df.columns)
 
+    # Convert dtypes, if possible
+
+    dtypes_mapping = get_dtypes(config)
+
+    for key,value in dtypes_mapping.items():
+        try:
+            if value == 'Int64':
+                df[key] = df[key].astype('Float64').astype('Int64')
+            else:
+                df[key] = df[key].astype(value)
+        except ValueError:
+            pass
+
     # Perform multiple processing steps to curate the dataset
 
-    df = tools.apply(df, config['processing'], indent=indent)
+    df = tools.apply(df, config['processing'], verbose=verbose, indent=indent)
 
     isvariable = ('variables' in config.keys())
 
@@ -231,45 +286,44 @@ def processing_data(df, config, init=False, indent=''):
     if isvariable and init:
         columns_after = set(df.columns)
         generated_columns = list(columns_after - columns_before)
-        config = update_config(config, list(df.columns), addcolumns=generated_columns)
+        config_updated = update_config(df, config, addcolumns=generated_columns)
 
     if isvariable:
 
         # Select the columns
 
-        print(indent + f'* dataframe')
-        print(indent + f'** columnselection')
-        print()
+        printv(f'* dataframe', verbose=verbose, indent=indent)
+        printv(f'** columnselection', verbose=verbose, indent=indent)
+        printv('', verbose=verbose, indent=indent)
 
-        colnames_mapping = rename_columns(config)
+        colnames_mapping = rename_columns(config_updated)
         df = df[list(colnames_mapping.keys())]
 
         # Apply dtype conversion
 
-        print(indent + f'* dataframe')
-        print(indent + f'** dtypeconversion')
-        print()
+        printv(f'* dataframe', verbose=verbose, indent=indent)
+        printv(f'** dtypeconversion', verbose=verbose, indent=indent)
+        printv('', verbose=verbose, indent=indent)
 
-        df = dtypeconversion(df, config, indent=indent)
+        df = dtypeconversion(df, config_updated, indent=indent + '   ')
 
         # Rename the columns
 
-        print(indent + f'* dataframe')
-        print(indent + f'** columnrenaming')
-        if not init:
-            print()
+        printv(f'* dataframe', verbose=verbose, indent=indent)
+        printv(f'** columnrenaming', verbose=verbose, indent=indent)
+        printv('', verbose=verbose)
 
         df = df.rename(columns=colnames_mapping)
 
     if init:
         colnames = list(df.columns)
-        Nlines = math.ceil(len(colnames)/2)
+        Nlines = math.ceil(len(colnames)/4)
         for line in range(Nlines):
-            string = indent + ' '.join(colnames[line*2:line*2+2])
-            print(string)
-        print()
+            string = indent + '   ' + ' '.join(colnames[line*4:line*4+4])
+            printv(string, verbose=verbose, indent=indent)
+        printv('', verbose=verbose, indent=indent)
 
-    return df, config
+    return df, config, config_updated
 
 
 if __name__ == '__main__':
@@ -287,19 +341,31 @@ if __name__ == '__main__':
 
     if ('inputfile_path' not in config.keys()):
         raise KeyError("`clean.py` | The configuration file must include a 'inputfile_path' section")
+    if (not os.path.isfile(config['inputfile_path'])):
+        raise FileNotFoundError(f"`clean.py` | No such file: '{config['inputfile_path']}'")
+    config['inputfile_path'] = expanduser(config['inputfile_path'])
     if ('inputdir_path' not in config.keys()):
         raise KeyError("`clean.py` | The configuration file must include a 'inputdir_path' section")
+    if (not os.path.exists(config['inputdir_path'])):
+        raise FileNotFoundError(f"`clean.py` | No such directory: '{config['inputdir_path']}'")
+    config['inputdir_path'] = expanduser(config['inputdir_path'])
     if ('outputdir_path' not in config.keys()):
         raise KeyError("`clean.py` | The configuration file must include a 'outputdir_path' section")
+    if (not os.path.exists(config['outputdir_path'])):
+        try:
+            os.mkdir(config['outputdir_path'])
+        except FileExistsError:
+            pass
+    config['outputdir_path'] = expanduser(config['outputdir_path'])
     if ('outputfile_path' not in config.keys()):
         raise KeyError("`clean.py` | The configuration file must include a 'outputfile_path' section")
     if ('processing' not in config.keys()):
         raise KeyError("`clean.py` | The configuration file must include a 'processing' section")
 
     if len(os.path.dirname(config['outputfile_path'])) == 0:
-        outputfile = os.path.join(config['outputdir_path'],config['outputfile_path'])
-     else:
-        outputfile = config['outputfile_path']
+        outputfile = expanduser(os.path.join(config['outputdir_path'],config['outputfile_path']))
+    else:
+        outputfile = expanduser(config['outputfile_path'])
 
     isvariable = ('variables' in config.keys())
     if not isvariable:
@@ -313,7 +379,7 @@ if __name__ == '__main__':
     # Set `stdnan` to False since all missing values in the database
     # will be standardized before any processing steps are applied
 
-    config = yaml.safe_load(re.sub("'stdnan': .*?,","'stdnan': False,",config))
+    config = yaml.safe_load(re.sub("'stdnan': .*?,","'stdnan': False,",str(config)))
 
     # If specified, apply the `marineloc` filter
 
@@ -322,16 +388,17 @@ if __name__ == '__main__':
     if len(marineloc_filter) > 1:
         raise Exception(f"`clean.py` | `marineloc` should be specified only once in the `config` file")
 
-    if len(isinworms_filter) == 1:
+    if len(marineloc_filter) == 1:
 
         marineloc_idx = marineloc_filter[0][0]
         marineloc_params = marineloc_filter[0][1]['tool'][0]['marineloc']
         marineloc_params['indent'] = '   '
 
-        print('Pre-processing')
+        print('Preprocessing')
         print('--------------')
         print()
-        print(f"* {marineloc_params['latkey']}, {marineloc_params['lonkey']}")
+        if ('latkey' in marineloc_params) and ('lonkey' in marineloc_params):
+            print(f"* {marineloc_params['latkey']}, {marineloc_params['lonkey']}")
         print('** marineloc')
         print()
 
@@ -359,8 +426,8 @@ if __name__ == '__main__':
         isinworms_params = config['processing'][isinworms_column_idx][isinworms_column][isinworms_idx]['isinworms']
         isinworms_args = list(isinworms_params.keys())
 
-        if 'outputpath' not in isinworms_args:
-            config['processing'][isinworms_column_idx][isinworms_column][isinworms_idx]['isinworms']['outputpath'] = config['inputdir_path']
+        if 'outputdir' not in isinworms_args:
+            config['processing'][isinworms_column_idx][isinworms_column][isinworms_idx]['isinworms']['outputdir'] = config['inputdir_path']
             isinworms_params = config['processing'][isinworms_column_idx][isinworms_column][isinworms_idx]['isinworms']
             isinworms_args = list(isinworms_params.keys())
 
@@ -383,8 +450,6 @@ if __name__ == '__main__':
 
         worms_matchfilter, worms_acceptedfilter = cwf.create_WoRMSfilter(**createwormsfilters_params)
 
-        print()
-
         ## Add the filters to `config`
 
         config['processing'][isinworms_column_idx][isinworms_column][isinworms_idx]['isinworms']['matchfilter'] = worms_matchfilter.copy(deep=True)
@@ -392,10 +457,6 @@ if __name__ == '__main__':
 
         del worms_matchfilter
         del worms_acceptedfilter
-
-    # Load specified dtypes
-
-    dtypes_mapping = get_dtypes(config)
 
     # Read the gzip text data file
 
@@ -416,6 +477,7 @@ if __name__ == '__main__':
         batch = 0
         data2clean = []
         error = []
+        config_updated = None
 
         start = time.time()
         for idx, line in enumerate(data):
@@ -423,7 +485,7 @@ if __name__ == '__main__':
             # Add observations
 
             obs = decode_line(line).strip('\n').split('\t')
-            obs = [preprocessquotationmark(value) for value in obs]
+            obs = [preprocessquotationmark.apply(value) for value in obs]
 
             if len(obs) == header_length:
                 data2clean.append(obs)
@@ -431,23 +493,25 @@ if __name__ == '__main__':
             else:
                 error.append(idx+2)
                 print()
-                print(f'    SplittingError: splitting line n°{idx+2} yields a different number of fields ({len(obs)}) than the header ({header_length}).')
-                print(f'                    line n°{idx+2} is skipped : {line}')
+                print(f'SplittingError: splitting line n°{idx+2} yields a different number of fields ({len(obs)}) than the header ({header_length}).')
+                print(f'line n°{idx+2} is skipped : {line}')
 
             if batch == BATCH_SIZE:
 
                 df2clean = pd.DataFrame(data2clean, columns=header)
-                df2clean = df2clean.astype(dtypes_mapping)
 
                 # Process data
 
-                print(f'Processing | {idx+1} lines')
-                df2clean, config = processing_data(df2clean, config, init=init, indent='')
+                print(f'--- Processing | {batch} lines ---')
                 print()
+                df2clean, config, config_updated = curate_data(df2clean, config, config_updated, init=init, verbose=True, indent='')
 
                 # Store data
 
-                writedataframe.to_txt(df2clean, outputfile, init=init, verbose=True, indent='')
+                if init:
+                    columns = list(df2clean.columns)
+
+                writedataframe.to_txt(df2clean[columns], outputfile, init=init, verbose=True, indent='')
 
                 end = time.time()
                 print(f'TIME : {np.round(end-start,0)}s')
@@ -461,21 +525,20 @@ if __name__ == '__main__':
     if batch != 0:
 
         df2clean = pd.DataFrame(data2clean, columns=header)
-        df2clean = df2clean.astype(dtypes_mapping)
 
         # Process data
 
+        print(f'--- Processing | {batch} lines ---')
         print()
-        print(f'Processing | {idx+1} lines done (end of file)')
-        df2clean, config = processing_data(df2clean, config, init=init, indent='')
+
+        df2clean, config, config_updated = curate_data(df2clean, config, config_updated, init=init, verbose=True, indent='')
 
         # Store data
 
-        writedataframe.to_txt(df2clean, outputfile, init=init, verbose=True, indent='')
+        writedataframe.to_txt(df2clean[columns], outputfile, init=init, verbose=True, indent='')
 
         end = time.time()
         print(f'TIME : {np.round(end-start,0)}s')
-        print()
 
     print()
     print(f"----- End cleaning {config['inputfile_path']} -----")
