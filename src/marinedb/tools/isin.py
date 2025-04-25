cf7d2f2482b17192e8005b184226a20d59b339be
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-25 16:10:43 +0200

    MODIFY apply() (add outputdir option, modify (minimize_flagname=True & flagname_mapping=None) case) in isin.py | marinedb/tools

--- src/marinedb/tools/isin.py
+++ src/marinedb/tools/isin.py
@@ -3,6 +3,8 @@
 
 # External import
 
+import os
+import json
 import pandas as pd
 
 # Internal import
@@ -18,7 +20,7 @@ from marinedb.tools import aligndtypes
 __all__ = [] # populated using the @export decorator
 
 @export
-def apply(df, key, values, flag=False, minimize_flagname=False, flagname_mapping=None, dropna=False, verbose=True, indent=''):
+def apply(df, key, values, flag=False, minimize_flagname=False, flagname_mapping=None, dropna=False, verbose=True, indent='', outputdir='./'):
 
     if (flagname_mapping is not None) and (len(flagname_mapping) == 0):
         flagname_mapping = None
@@ -41,8 +43,27 @@ def apply(df, key, values, flag=False, minimize_flagname=False, flagname_mapping
                 raise ValueError('`isin.py` | if a string, `flagname_mapping` must be a path to a valid JSON file')
 
     if minimize_flagname and (flagname_mapping is None):
-        flagname_mapping = {val: idx for idx, val in enumerate(values)}
+        outputfile = os.path.join(outputdir,f'{key}_isin_mapping.json')
+        if os.path.isfile(outputfile):
+            printv(f'INFO | {outputfile} already exists and will be used', verbose=verbose, indent=indent)
+            with open(outputfile,'r') as file:
+                flagname_mapping = json.load(file)
+            temp = {}
+            for k,v in flagname_mapping.items():
+                temp[str(k)] = int(v)
+            flagname_mapping = temp
+            start_idx = max(list(flagname_mapping.values())) + 1
+        else:
+            flagname_mapping = {}
+            start_idx = 0
+        value_str = [str(val) for val in values]
+        value_update = list(set(value_str) - set(list(flagname_mapping.keys())))
+        value_update = {val: (start_idx + idx) for idx, val in enumerate(value_update)}
+        flagname_mapping.update(value_update)
         printv(f'INFO | `flagname_mapping` is set to {flagname_mapping}', verbose=verbose, indent=indent)
+        printv(f'INFO | Save `flagname_mapping` to {outputfile}', verbose=verbose, indent=indent)
+        with open(outputfile, 'w', encoding='utf-8') as file:
+            json.dump(flagname_mapping, file, ensure_ascii=False, indent=4)
 
     df, key, _ = getcolumnname.apply(df, key, '', inplace=True)
 
