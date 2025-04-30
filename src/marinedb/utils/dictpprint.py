a951703c13499c7ded8d031f5ac60445e42f5624
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-30 15:37:22 +0200

    CREATE dictpprint.py | marinedb/utils

---
+++ src/marinedb/utils/dictpprint.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# Global variable
+
+__all__ = ['apply']
+
+def apply(dictionary, verbose=True, indent=''):
+
+    if verbose:
+        print(indent + '{')
+        print('\n'.join(indent + ' ' + '{}: {}'.format(k, v) for k, v in dictionary.items()))
+        print(indent + '}')
+
+    return True
