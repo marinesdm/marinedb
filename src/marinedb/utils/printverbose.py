c2a41c7413eaa86ef0e20b981973bb7a9c6c23e1
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-04-30 15:35:59 +0200

    ADD numpy import ; MODIFY printv() (add width option) ; in printverbose.py | marinedb/utils

--- src/marinedb/utils/printverbose.py
+++ src/marinedb/utils/printverbose.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 # coding: utf-8
 
+# External import
+
+import numpy as np
+
 # Global variable
 
 __all__ = ['printv']
 
-def printv(message, verbose, indent=''):
+def printv(message, verbose, indent='', width=150):
+
     if verbose:
-        print(indent + message)
+
+        width = width - len(indent)
+        message_split = np.array(message.split(' '))
+        while len(message_split) > 0:
+            message_cumlength = np.cumsum([len(string) for string in message_split])
+            condition = (message_cumlength <= width)
+            if not condition.any():
+                condition = np.array([True] + [False]*(len(message_cumlength) - 1))
+            print(indent + ' '.join(message_split[condition]))
+            message_split = message_split[~condition]
+
     return True
