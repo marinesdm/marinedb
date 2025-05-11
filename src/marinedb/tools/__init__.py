78e3775eb181e31a89a7ae26b324d4a0e13225af
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2025-05-11 19:02:39 +0200

    FIX (modify colname, modify proc_params) & MODIFY (rename outputfile_marinedb option, add outputdir_marinedb option) apply() in __init__.py | marinedb/tools

--- src/marinedb/tools/__init__.py
+++ src/marinedb/tools/__init__.py
@@ -22,13 +22,17 @@ from marinedb.utils.printverbose import printv
 __all__ = ['contains', 'doesnotcontain','dropvalues','isboundedby','isin','isna','notisin','doeslateqlon','isbelow_minlatlonprecision','iszero','lettersonly','taxasubset','parsedate','processdateinterval','splitdate','temporal']
 
 
-def apply(df, config_dict, verbose=True, indent='', store_stats=True, outputfile_stats='cleaning_stats.txt'):
+def apply(df, config_dict, verbose=True, indent='', store_stats=True, outputdir_marinedb='./', outputfile_marinedb='marinedb_stats.txt'): #,i=None): #DEBUG NONE !!
+
+    if len(os.path.dirname(outputfile_marinedb)) == 0:
+        outputfile_marinedb = os.path.join(outputdir_marinedb, outputfile_marinedb)
 
     header = []
     stats = []
 
     for procstep in config_dict:
-
+#        if i is not None:
+#            print('here' + str(i)) #debug
         colname = list(procstep.keys())[0]
 
         for proc in procstep[colname]:
@@ -42,11 +46,14 @@ def apply(df, config_dict, verbose=True, indent='', store_stats=True, outputfile
                 proc_name = proc
                 proc_params = {}
 
+            proc_params['verbose'] = verbose
+            proc_params['indent'] = indent + '   '
+
             length_before=len(df)
 
             if colname == 'tool':
 
-                colname = [key for key in proc_params.keys() if 'key' in key]
+                colname = [value for key, value in proc_params.items() if 'key' in key]
                 if len(colname) != 0:
                     printv(f'* {", ".join(colname)}', verbose=verbose, indent=indent)
                     procstep_string = f'{"-".join(colname)}'
@@ -54,25 +61,23 @@ def apply(df, config_dict, verbose=True, indent='', store_stats=True, outputfile
                     printv(f'* dataframe', verbose=verbose, indent=indent)
                     procstep_string = 'dataframe'
                 printv(f'** {proc_name}', verbose=verbose, indent=indent)
-#                printv('', verbose=verbose, indent=indent)
+
                 procstep_string = '_'.join([procstep_string, proc_name])
 
-                proc_params['indent'] = indent + '   '
                 df = eval(f"{proc_name}.apply(df, **proc_params)")
 
             else:
 
                 printv(f'* {colname}', verbose=verbose, indent=indent)
                 printv(indent + f'** {proc_name}', verbose=verbose, indent=indent)
-#                printv('', verbose=verbose, indent=indent)
+
                 procstep_string = '_'.join([colname, proc_name])
 
-                proc_params['indent'] = indent + '   '
                 df = eval(f"{proc_name}.apply(df, colname, **proc_params)")
 
-#            printv('', verbose=verbose, indent=indent)
             length_after = len(df)
             printv(f'{proc_name} | before: {length_before}, after: {length_after}', verbose=verbose, indent=indent + '   ')
+
             if store_stats:
                 header += [f'{procstep_string}' + '_before', f'{procstep_string}' + '_after']
                 stats += [length_before, length_after]
@@ -85,10 +90,10 @@ def apply(df, config_dict, verbose=True, indent='', store_stats=True, outputfile
 
     if store_stats:
         stats = pd.DataFrame([stats], columns=header, dtype=int)
-        if os.path.isfile(outputfile_stats):
-            stats.to_csv(outputfile_stats, sep='\t', index=False, header=False, mode='a')
+        if os.path.isfile(outputfile_marinedb):
+            stats.to_csv(outputfile_marinedb, sep='\t', index=False, header=False, mode='a')
         else:
-            stats.to_csv(outputfile_stats, sep='\t', index=False)
+            stats.to_csv(outputfile_marinedb, sep='\t', index=False)
 
     return df
 
