e019a4d8246a86424a7d7e73af882bca3ac37e8e
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2026-05-13 16:27:53 +0200

    ADD import (ListedColormap) ; MODIFY __main__ (support grayscale-style export alongside viridis rendering) ; in plot_globe_mask_coastline.py | marinedb/tools/data

--- src/marinedb/tools/data/plot_globe_mask_coastline.py
+++ src/marinedb/tools/data/plot_globe_mask_coastline.py
@@ -1,13 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 if __name__=='__main__':
+
+    black_and_white = True
 
     mask = np.load('globe_mask_coastline.npz')
     mask = mask['mask']
 
+    if black_and_white:
+
+        colors = [
+            '#F5F3EE', # land
+            '#CDD3D1', # sea
+            '#B86A63'  # coast
+        ]
+
+        cmap = ListedColormap(colors)
+
+        filename = 'globe_mask_coastline_gray'
+
+    else:
+
+        cmap = plt.cm.viridis
+
+        filename = 'globe_mask_coastline_viridis'
+
     fig = plt.figure(figsize=(30,20))
-    cmap = plt.cm.viridis
+
     plt.imshow(mask, vmin=0, vmax=2, cmap=cmap)
     plt.axis('off')
-    plt.savefig('globe_mask_coastline', bbox_inches='tight',dpi=96)
+    plt.savefig(filename, bbox_inches='tight',dpi=96)
