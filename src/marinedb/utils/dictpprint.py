8e575f9ad1751689dd5eddec7212301ca9942d48
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2026-05-13 11:43:51 +0200

    MODIFY apply() (add key_order arg, allow controlled display order for dictionary entries) in dictpprint.py | marinedb/utils

--- src/marinedb/utils/dictpprint.py
+++ src/marinedb/utils/dictpprint.py
@@ -5,11 +5,14 @@
 
 __all__ = ['apply']
 
-def apply(dictionary, verbose=True, indent=''):
+def apply(dictionary, key_order=None, verbose=True, indent=''):
+
+    if not key_order:
+        key_order = dictionary.keys()
 
     if verbose:
         print(indent + '{')
-        print('\n'.join(indent + ' ' + '{}: {}'.format(k, v) for k, v in dictionary.items()))
+        print('\n'.join(indent + ' ' + '{}: {}'.format(k, dictionary[k]) for k in key_order))
         print(indent + '}')
 
     return None
