f625e4cf7072eb880ade972b8708039b93f1b64e
Berhocoirigoin Elena <eberhocoi@georgiatech-metz.fr>
2026-05-13 11:45:57 +0200

    MODIFY printv() (change default width value) in printverbose.py [minor changes] | marinedb/utils

--- src/marinedb/utils/printverbose.py
+++ src/marinedb/utils/printverbose.py
@@ -9,18 +9,23 @@ import numpy as np
 
 __all__ = ['printv']
 
-def printv(message, verbose, indent='', width=150):
+def printv(message, verbose, indent='', width=125):
 
     if verbose:
 
         width = width - len(indent)
         message_split = np.array(message.split(' '))
+
         while len(message_split) > 0:
+
             message_cumlength = np.cumsum([len(string) for string in message_split])
             condition = (message_cumlength <= width)
+
             if not condition.any():
                 condition = np.array([True] + [False]*(len(message_cumlength) - 1))
+
             print(indent + ' '.join(message_split[condition]))
+
             message_split = message_split[~condition]
 
     return True
